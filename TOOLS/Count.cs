c7665fef8f01c76d2f596f4199d3ca6c13c0ed10
VaclavKey <152690177+VaclavKey@users.noreply.github.com>
2024-06-24 14:12:08 +0300

    Create Count.cs

---
+++ TOOLS/Count.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using static hackathon_final_build.Globals;
+
+
+namespace hackathon_prefinal_build
+{
+    internal class Count
+    {
+        public static int Buildings()
+        {
+            int count = 0;
+
+            SqlConnection connection = new(connectionSql);
+            string sqlException = $"SELECT * FROM Building";
+
+
+            using (connection)
+            {
+                connection.Open();
+
+                SqlCommand command = new(sqlException, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public static int Tasks()
+        {
+            int count = 0;
+
+            SqlConnection connection = new(connectionSql);
+            string sqlException = $"SELECT * FROM Tasks";
+
+
+            using (connection)
+            {
+                connection.Open();
+
+                SqlCommand command = new(sqlException, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public static int Tasks(int ID)
+        {
+            int count = 0;
+
+            SqlConnection connection = new(connectionSql);
+            string sqlException = $"SELECT * FROM Tasks WHERE ID_Building = {ID}";
+
+
+            using (connection)
+            {
+                connection.Open();
+
+                SqlCommand command = new(sqlException, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
